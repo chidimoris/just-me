bb60c0bebb8944003e6dceaa46d5f1a5c01921da
chidimoris <saintmoris2dey2018@gmail.com>
2022-10-29 19:26:46 +0100

    my javascript assignment

---
+++ script.js
@@ -0,0 +1,12 @@
+fahrenheit;
+var fahrenheit=56;
+var iyana=(fahrenheit-32)*5/9;
+function tocelsuis(fahrenheit){
+//     var fahrenheit=56
+// var ftocel=(f-32)*5/9;
+var lag="My location is iyana-ipaja in Alimosho Local Government,Lagos State " + iyana
+document.getElementById("vert").innerHTML=lag
+document.getElementById("vert").style.color="darkblue"
+document.getElementById("vert").style.background="pink"
+}
+console.log(iyana)
